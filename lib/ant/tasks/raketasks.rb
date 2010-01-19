9c185b193bbe0e17cea76caad04b656ef4e240be
Thomas E. Enebo <tom.enebo@gmail.com>
2010-01-19 15:32:34 -0600

    Minor refactoring

--- lib/ant/tasks/raketasks.rb
+++ lib/ant/tasks/raketasks.rb
@@ -3,24 +3,26 @@ require 'rake'
 
 class RakeWrapper
   def load_tasks(*args)
-    application = Rake::Application.new
-    Rake.application = application
+    # FIXME: Use our arguments (this sucks...let's submit a patch for Rake
     ARGV.clear
-    args.each {|a| ARGV << a }
-    application.init
-    application.load_rakefile
-    application
+    ARGV.concat args
+
+    Rake.application.tap do |application|
+      application.init
+      application.load_rakefile
+    end
   end
 
   def execute(*args)
-    application = load_tasks(*args)
-    application.top_level
+    load_tasks(*args).top_level
   end
 
   def invoke_task(task)
     Rake.application[task].invoke
   end
 
-  def import(filename)
+  def import(*args)
+    application = load_tasks(*args)
+    application
   end
 end
