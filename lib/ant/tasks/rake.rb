356d9cec878371b09006bb17899833355aa7250a
Nick Sieger <nick@nicksieger.com>
2010-01-19 14:45:26 -0600

    Split up load and execute

--- lib/ant/tasks/rake.rb
+++ lib/ant/tasks/rake.rb
@@ -2,10 +2,23 @@ require 'rubygems'
 require 'rake'
 
 class RakeWrapper
-  def execute(*args)
-    application = Rake.application
+  def load_tasks(*args)
+    application = Rake::Application.new
+    Rake.application = application
     ARGV.clear
     args.each {|a| ARGV << a }
-    application.run
+    application.init
+    application.load_rakefile
+    application
+  end
+
+  def execute(*args)
+    application = load_tasks(*args)
+    application.top_level
+  end
+
+  def invoke_task(task)
+    application = load_tasks
+    application[task].invoke
   end
 end
