748ad0f30c8acc68e21d8d027ecdde1da349134b
Thomas E. Enebo <tom.enebo@gmail.com>
2010-01-25 14:01:36 -0600

    Update ant_task to use new 'ant' method for calling

--- lib/ant/rake.rb
+++ lib/ant/rake.rb
@@ -1,6 +1,5 @@
 def ant_task(*args, &block)
-  @ant ||= Ant.new
   task = task(*args, &block)
-  @ant.add_target task
-  @ant.execute_target task.name
+  ant.add_target task
+  ant.execute_target task.name
 end
